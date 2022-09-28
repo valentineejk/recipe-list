633a8609c8805159b678dc868039835626e346eb
valentineejk <valentineejk@gmail.com>
2022-09-28 15:45:46 +0100

    Initial Commit

---
+++ recipe-list/recipe_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_listApp.swift
+//  recipe-list
+//
+//  Created by Valentineejk on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct recipe_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
